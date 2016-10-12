5b97e155c6b1e8479984e058d799c68bda9a406f
silvanicus <silvana.ramirez85@gmail.com>
2016-10-12 18:45:47 -0500

    Json details

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,17 @@
-function fetchJSONFile(path, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
-            }
-        }
-    };
-    httpRequest.open('GET', path);
-    httpRequest.send(); 
-}
 
 $(function(){
 
-
-
-	fetchJSONFile('http://silvanicus.github.io/assets/json/app.json', function(data){
-	    var myData = data;
+	$.getJSON( "http://silvanicus.github.io/assets/json/app.json", function( data ) {
+	  var items = [];
+	  $.each( data, function( key, val ) {
+	    items.push( "<li id='" + key + "'>" + val + "</li>" );
+	  });
+	 
+	  $( "<ul/>", {
+	    "class": "my-new-list",
+	    html: items.join( "" )
+	  }).appendTo( "body" );
 	});
-
 
 	//Preload images + spinner
     $(window).load(function(){
