f763b3ba32e0006ad2c42781f3e1993d4374cb25
silvanicus <silvana.ramirez85@gmail.com>
2016-10-12 18:12:09 -0500

    json test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,26 @@
 $(function(){
 
+	function fetchJSONFile(path, callback) {
+	    var httpRequest = new XMLHttpRequest();
+	    httpRequest.onreadystatechange = function() {
+	        if (httpRequest.readyState === 4) {
+	            if (httpRequest.status === 200) {
+	                var data = JSON.parse(httpRequest.responseText);
+	                if (callback) callback(data);
+	            }
+	        }
+	    };
+	    httpRequest.open('GET', path);
+	    httpRequest.send(); 
+	}
+
+	// this requests the file and executes a callback with the parsed result once
+	//   it is available
+	fetchJSONFile('http://silvanicus.github.io/assets/json/app.json', function(data){
+	    // do something with your data
+	    console.log(data);
+	});
+
 	//Preload images + spinner
     $(window).load(function(){
         $('.cover').fadeOut(5000);
